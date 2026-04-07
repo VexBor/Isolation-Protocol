d68a038f2424e5c8ce8f3406b982b13f35598faa
Vadim <vadimakim2@gmail.com>
2026-04-07 17:44:35 +0300

    add hero struckture and swap logic

---
+++ Models/Hero.cs
@@ -0,0 +1,12 @@
+namespace Isolation_Protocol.Models;
+
+public class Hero
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string ImagePath { get; set; }
+    public int Health { get; set; }
+    public int Stamina { get; set; }
+    public int Armor { get; set; }
+    public int Strength { get; set; }
+}
--- Views/HeroView.axaml.cs
+++ Views/HeroView.axaml.cs
@@ -1,13 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using Isolation_Protocol.Models;
 
 namespace Isolation_Protocol.Views;
 
 public partial class HeroView : UserControl
 {
+    private string _heroPath = "heroes.json";
+    private List<Hero>? heroes = new List<Hero>();
+    private string? _assemblyName;
+
     public HeroView()
     {
         InitializeComponent();
+
+        _assemblyName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+
+        if (File.Exists(_heroPath))
+        {
+            var str = File.ReadAllText(_heroPath);
+            heroes = JsonSerializer.Deserialize<List<Hero>>(str);
+        }
+    }
+
+    private void Select_Button(object? sender, RoutedEventArgs e)
+    {
+        if (sender is Button clickedButton)
+        {
+            var targetHero = heroes.FirstOrDefault(hero => hero.Name + "Button" == clickedButton.Name);
+
+            if (targetHero != null)
+            {
+                var uri = new Uri($"avares://{_assemblyName}/{targetHero.ImagePath}");
+                
+                selectedHeroImage.Source = new Bitmap(AssetLoader.Open(uri));
+                heroNameText.Text = targetHero.Name;
+                heroHealthText.Text = $"HP: {targetHero.Health}";
+                heroStaminaText.Text = $"Stamina: {targetHero.Stamina}";
+                heroArmorText.Text = $"Armor: {targetHero.Armor}";
+                heroStrengthText.Text = $"Strength: {targetHero.Strength}";
+            }
+        }
     }
 }
